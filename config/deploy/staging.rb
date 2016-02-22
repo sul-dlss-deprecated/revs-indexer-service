c4d28aa0af1c82f30818041cceb24dea45b0715a
Peter Mangiafico <peter@mangiafico.org>
2016-02-22 15:40:16 -0800

    fix typo in development script server names

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,6 +1,6 @@
 set :bundle_without, %w{deployment test development}.join(' ')
 set :rails_env, "production"
-set :deploy_host, "revs-indexing-dev"
+set :deploy_host, "revs-indexing-stage"
 server "#{fetch(:deploy_host)}.stanford.edu", user: fetch(:user), roles: %w{web db app}
 
 Capistrano::OneTimeKey.generate_one_time_key!
