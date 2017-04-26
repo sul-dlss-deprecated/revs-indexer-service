448e554ba8e9c6866ac8bffdddbb1d8b6e238ae7
Peter Mangiafico <peter@mangiafico.org>
2017-04-26 12:33:11 -0700

    update to rails 5; use new settings format for targets; remove squash; add honeybadger; add okcomputer; fix deploy config linking

--- config/deploy/dev.rb
+++ config/deploy/dev.rb
@@ -1,13 +1,4 @@
 set :bundle_without, %w{deployment test}.join(' ')
 set :rails_env, "development"
-set :deploy_host, "revs-indexing-dev"
-server "#{fetch(:deploy_host)}.stanford.edu", user: fetch(:user), roles: %w{web db app}
-
 Capistrano::OneTimeKey.generate_one_time_key!
 
-namespace :deploy do
-  namespace :assets do
-    task :symlink do ; end
-    task :precompile do ; end
-  end
-end
--- config/deploy/stage.rb
+++ config/deploy/stage.rb
@@ -1,6 +1,3 @@
 set :bundle_without, %w{deployment test development}.join(' ')
 set :rails_env, "production"
-set :deploy_host, "revs-indexing-stage"
-server "#{fetch(:deploy_host)}.stanford.edu", user: fetch(:user), roles: %w{web db app}
-
 Capistrano::OneTimeKey.generate_one_time_key!
