5f8b5135a84b42b037293f3c7576e57774017a56
Peter Mangiafico <peter@mangiafico.org>
2017-06-19 13:36:47 -0700

    standardize cap deploy

--- config/deploy/dev.rb
+++ config/deploy/dev.rb
@@ -1,4 +1,6 @@
 set :bundle_without, %w{deployment test}.join(' ')
 set :rails_env, "development"
+server "revs-indexing-dev.stanford.edu", user: 'harvestdor', roles: %w{web db app}
+
 Capistrano::OneTimeKey.generate_one_time_key!
 
--- config/deploy/stage.rb
+++ config/deploy/stage.rb
@@ -1,3 +1,5 @@
 set :bundle_without, %w{deployment test development}.join(' ')
 set :rails_env, "production"
+server "revs-indexing-stage.stanford.edu", user: 'harvestdor', roles: %w{web db app}
+
 Capistrano::OneTimeKey.generate_one_time_key!
